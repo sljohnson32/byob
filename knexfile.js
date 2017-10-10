5eec193738c8738b0d186271f6a952d52e650c8b
Dan Alvarez <danalvarez5280@gmail.com>
2017-10-09 18:35:08 -0600

    fixed knex file connection

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost3000/school_finder',
+    connection: 'postgres://localhost/school_finder',
     migration: {
       directory: './db/migrations'
     },
