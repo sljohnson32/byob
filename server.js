ff2c9c0027fc244422cca3962bec7764c89f6149
Dan Alvarez <danalvarez5280@gmail.com>
2017-10-10 15:57:08 -0600

    able to GET specific school, district or county based on name parameter

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (request, response) => {
   response.send('School/s in session sucka!');
 });
 
-app.get('/schools', (request, response) => {
+app.get('/api/v1/schools', (request, response) => {
   database('schools').select()
   .then((schools) => {
     response.status(200).json(schools)
@@ -25,7 +25,7 @@ app.get('/schools', (request, response) => {
   });
 });
 
-app.get('/districts', (request, response) => {
+app.get('/api/v1/districts', (request, response) => {
   database('districts').select()
   .then((districts) => {
     response.status(200).json(districts)
@@ -35,26 +35,76 @@ app.get('/districts', (request, response) => {
   });
 });
 
-app.get('/counties', (request, response) => {
+app.get('/api/v1/counties', (request, response) => {
   database('counties').select()
   .then((counties) => {
     response.status(200).json(counties)
   })
   .catch((error) => {
-    response.status(404).json({error})
+    response.status(404).json({ error })
+  });
+});
+
+app.get('/api/v1/counties/:name', (request, response) => {
+  const name = request.params.name.toString().toUpperCase()
+  console.log('county name', name);
+
+  database('counties').where('name',  name).select()
+  .then((county) => {
+    response.status(200).json(county)
+  })
+  .catch((error) => {
+    response.status(404).json({
+      error: `Could not find county with name ${request.params.name}`
+    })
+  });
+});
+
+app.get('/api/v1/districts/:name', (request, response) => {
+  const name = request.params.name.toString().toUpperCase();
+
+  database('districts').where('name',  name).select()
+  .then((county) => {
+    response.status(200).json(county)
+  })
+  .catch((error) => {
+    response.status(404).json({
+      error: `Could not find county with name ${request.params.name}`
+    })
+  });
+});
+
+//notworking yet
+// app.get('/api/v1/districts/:code', (request, response) => {
+//   const code = request.params.code;
+//   database('districts').where('district_code', code).select()
+//   .then((county) => {
+//     response.status(200).json(county)
+//   })
+//   .catch((error) => {
+//     response.status(404).json({
+//       error: `Could not find county with code ${request.params.code}`
+//     })
+//   });
+// });
+
+app.get('/api/v1/schools/:name', (request, response) => {
+  const name = request.params.name.toString().toUpperCase();
+
+  database('schools').where('name',  name).select()
+  .then((county) => {
+    response.status(200).json(county)
+  })
+  .catch((error) => {
+    response.status(404).json({
+      error: `Could not find county with name ${request.params.name}`
+    })
   });
 });
 
 
-//not working yet
-app.get('/api/schools/:name', (request, response) => {
-  database('schools').where('name', request.params.name).select()
-  response.json({
-    name: request.params.name
-  });
-});
 
-//posts
+//posts -- not sure how we will even make a post at this point--
 app.post('/api/v1/schools', (request, response) => {
   const school = request.body;
 
@@ -113,6 +163,24 @@ app.post('/api/v1/counties', (request, response) => {
     .catch(error => {
       response.status(500).json({ error });
     });
+});
+
+//delete
+app.delete('/api/v1/schools/:id', (request, response) => {
+  const { id } = request.params;
+  console.log('id: ', id);
+
+  database('schools').where({ id }).del()
+  .then(palette => {
+    if (palette) {
+      response.sendStatus(204)
+    } else {
+      response.status(422).json({ error: 'Not Found' })
+    }
+  })
+  .catch(error => {
+    response.status(500).json({ error })
+  })
 });
 
 app.listen(app.get('port'), () => {
