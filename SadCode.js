2a7bac18a966c1c1aeaf95eae160445aee5635fe
DanAlvarez5280 <danalvarez5280@gmail.com>
2017-10-13 12:36:17 -0600

    Create SadCode.js

---
+++ SadCode.js
@@ -0,0 +1,13 @@
+const setJWTs = () => {
+  chai.request(server)
+    .post('/api/v1/authentication')
+    .send({ email: 'sam@turing.io', appName: 'byob' })
+    .end((error, response) => adminToken = JSON.parse(response.text));
+
+  chai.request(server)
+    .post('/api/v1/authentication')
+    .send({ email: 'sam@ricknmorty.com', appName: 'byob' })
+    .end((error, response) => regToken = JSON.parse(response.text));
+};
+
+//not sure if we could have done more to authenticate and or check authorization.
