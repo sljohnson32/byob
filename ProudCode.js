8e3ba66c5f5d6c580be4cb0b9ac9297054931eae
DanAlvarez5280 <danalvarez5280@gmail.com>
2017-10-13 12:30:48 -0600

    Create ProudCode.js

---
+++ ProudCode.js
@@ -0,0 +1,27 @@
+app.get('/api/v1/schools', (request, response) => {
+  let ratioMin = request.query.ratioMin;
+  let ratioMax = request.query.ratioMax;
+
+  const checkQuery = () => {
+    if (ratioMin && ratioMax) {
+      return database('schools').where('student_teacher_ratio', '>=', ratioMin).where('student_teacher_ratio', '<=', ratioMax).select();
+    }
+    if (ratioMin && !ratioMax) {
+      return database('schools').where('student_teacher_ratio', '>=', ratioMin).select();
+    }
+    if (!ratioMin && ratioMax) {
+      return database('schools').where('student_teacher_ratio', '<=', ratioMax).select();
+    }
+    if (!ratioMin && !ratioMax) {
+      return database('schools').select();
+    }
+  };
+
+  checkQuery()
+    .then((schools) => {
+      return response.status(200).json(schools);
+    })
+    .catch((error) => {
+      response.status(500).json({error});
+    });
+});
